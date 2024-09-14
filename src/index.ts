712f11203e9494011cbe7ea84921ebfba31138e5
shandyaillustrations <shandyaillustrations@gmail.com>
2024-09-15 00:50:58 +0700

    fix: cors missing allow origin

--- src/index.ts
+++ src/index.ts
@@ -30,6 +30,16 @@ interface Portfolio {
 
 app.use(bodyParser.json());
 
+// CORS
+
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*'); // allow all domains
+  res.setHeader('Access-Control-Allow-Methods', 'GET, PUT');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+
+  next();
+});
+
 app.get("/", (req: Request, res: Response) => {
   res.send("Express + TypeScript Server");
 });
